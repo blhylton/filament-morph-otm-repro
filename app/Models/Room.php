bd137f5d058abb59e5a8917022d540ef748e32af
Barry Hylton <bhylton@designsensory.com>
2024-07-12 13:09:55 -0400

    Create rooms table

---
+++ app/Models/Room.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property-read int $id
+ */
+class Room extends Model
+{
+    use HasFactory;
+}
