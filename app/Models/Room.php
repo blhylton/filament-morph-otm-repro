22bf8357c5cde54f2561a84d3219a0a574324599
Barry Hylton <bhylton@designsensory.com>
2024-07-12 13:18:14 -0400

    Add relations to Room

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 /**
  * @property-read int $id
@@ -11,4 +12,22 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+
+    public function oneWeekProduct(): MorphOne
+    {
+        return $this->morphOne(Product::class, 'productable')
+            ->ofMany([], fn ($q) => $q->where('duration', 7));
+    }
+
+    public function twoWeekProduct(): MorphOne
+    {
+        return $this->morphOne(Product::class, 'productable')
+            ->ofMany([], fn ($q) => $q->where('duration', 14));
+    }
+
+    public function weekendProduct(): MorphOne
+    {
+        return $this->morphOne(Product::class, 'productable')
+            ->ofMany([], fn ($q) => $q->where('duration', 2));
+    }
 }
