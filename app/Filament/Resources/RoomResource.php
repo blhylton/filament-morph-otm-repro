64634588296fb98ccf8060354c2a908d4258b922
Barry Hylton <bhylton@designsensory.com>
2024-07-12 13:30:03 -0400

    Create filament resources

---
+++ app/Filament/Resources/RoomResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RoomResource\Pages;
+use App\Filament\Resources\RoomResource\RelationManagers;
+use App\Models\Room;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class RoomResource extends Resource
+{
+    protected static ?string $model = Room::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('oneWeekProduct')
+                    ->rules('exists:products,id')
+                    ->label('One Week Product')
+                    ->relationship(
+                        name: 'oneWeekProduct',
+                        titleAttribute: 'name',
+                    ),
+                Select::make('twoWeekProduct')
+                    ->rules('exists:products,id')
+                    ->label('Two Week Product')
+                    ->relationship(
+                        name: 'twoWeekProduct',
+                        titleAttribute: 'name',
+                    ),
+                Select::make('weekendProduct')
+                    ->rules('exists:products,id')
+                    ->label('Weekend Product')
+                    ->relationship(
+                        name: 'weekendProduct',
+                        titleAttribute: 'name',
+                    ),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRooms::route('/'),
+            'create' => Pages\CreateRoom::route('/create'),
+            'edit' => Pages\EditRoom::route('/{record}/edit'),
+        ];
+    }
+}
